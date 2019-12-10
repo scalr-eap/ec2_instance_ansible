974fda1ea2f91f76d4af6e67cd2106a2a70b7270
Ryan <ryan.fee625@gmail.com>
2019-12-10 15:42:21 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "scalr_aws_secret_key" {}
+
+variable "scalr_aws_access_key" {}
+
+variable "region" {
+  description = "Region"
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+}
+
+variable "subnet" {
+description = "Subnet ID"
+}
+
+variable "sg" {
+  description = "AWS Secruity Group"
+  type        = list(string)
+}
+
+variable "key" {
+  description = "AWS Key"
+}
+
+variable "vpc_id" {
+description = "VPC"
+}
+
+variable "ami" {
+  description = "AMI"
+}
+
+variable "remote_host" {
+  description = "Ansible Host"
+}
+
+variable "ssh_private_key" {
+  description = "The text of SSH Private key. This will be formatted by the Terraform template.<br>This will be used in the remote workspace to allow Terraform to connect to the servers and run scripts to configure Scalr. It only exists in the workspace for the duration of the run."
+  type = string
+  default = "FROM_FILE"
+}
